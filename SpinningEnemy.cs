891d57a64990ea3bf093c3655b653f9d41ab5c41
Demiray Ustaoglu <demirayustaoglu@pro-line.com.tr>
2016-09-23 08:53:45 +0300

    Add project files.

---
+++ SpinningEnemy.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace _3D_Game
+{
+    class SpinningEnemy : BasicModel
+    {
+        Matrix rotation = Matrix.Identity;
+
+        public SpinningEnemy(Model m)
+            : base(m)
+        {
+        }
+
+        public override void Update()
+        {
+            rotation *= Matrix.CreateRotationY(MathHelper.Pi / 180);
+        }
+
+        public override Matrix GetWorld()
+        {
+            return world * rotation;
+        }
+    }
+}
