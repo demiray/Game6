df00220efaa0eb99c4bae39909c2e7df8b323efd
Demiray Ustaoglu <demiray.ustaoglu@pro-line.com.tr>
2016-09-29 22:50:08 +0300

    ready for ch12

--- Camera.cs
+++ Camera.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
@@ -15,9 +16,15 @@ namespace _3D_Game
         Vector3 cameraDirection;
         Vector3 cameraUp;
 
-        float speed = 3;
+        //float speed = 3;
 
         MouseState prevMouseState;
+
+        // Max yaw/pitch variables
+        float totalYaw = MathHelper.PiOver4 / 2;
+        float currentYaw = 0;
+        float totalPitch = MathHelper.PiOver4 / 2;
+        float currentPitch = 0;
 
         public Camera(Game game, Vector3 pos, Vector3 target, Vector3 up)
             : base(game)
@@ -51,47 +58,69 @@ namespace _3D_Game
         public override void Update(GameTime gameTime)
         {
             // TODO: Add your update code here
-            // Move forward/backward
-            if (Keyboard.GetState().IsKeyDown(Keys.W))
-                cameraPosition += cameraDirection * speed;
-            if (Keyboard.GetState().IsKeyDown(Keys.S))
-                cameraPosition -= cameraDirection * speed;
+            //// Move forward/backward
+            //if (Keyboard.GetState().IsKeyDown(Keys.W))
+            //    cameraPosition += cameraDirection * speed;
+            //if (Keyboard.GetState().IsKeyDown(Keys.S))
+            //    cameraPosition -= cameraDirection * speed;
 
-            // Move side to side
-            if (Keyboard.GetState().IsKeyDown(Keys.A))
-                cameraPosition += Vector3.Cross(cameraUp, cameraDirection) * speed;
-            if (Keyboard.GetState().IsKeyDown(Keys.D))
-                cameraPosition -= Vector3.Cross(cameraUp, cameraDirection) * speed;
+            //// Move side to side
+            //if (Keyboard.GetState().IsKeyDown(Keys.A))
+            //    cameraPosition += Vector3.Cross(cameraUp, cameraDirection) * speed;
+            //if (Keyboard.GetState().IsKeyDown(Keys.D))
+            //    cameraPosition -= Vector3.Cross(cameraUp, cameraDirection) * speed;
 
+
+            //// Yaw rotation
+            //cameraDirection = Vector3.Transform(cameraDirection,
+            //Matrix.CreateFromAxisAngle(cameraUp, (-MathHelper.PiOver4 / 150) *
+            //(Mouse.GetState().X - prevMouseState.X)));
 
             // Yaw rotation
-            cameraDirection = Vector3.Transform(cameraDirection,
-            Matrix.CreateFromAxisAngle(cameraUp, (-MathHelper.PiOver4 / 150) *
-            (Mouse.GetState().X - prevMouseState.X)));
+            float yawAngle = (-MathHelper.PiOver4 / 150) *
+            (Mouse.GetState().X - prevMouseState.X);
+            if (Math.Abs(currentYaw + yawAngle) < totalYaw)
+            {
+                cameraDirection = Vector3.Transform(cameraDirection,
+                Matrix.CreateFromAxisAngle(cameraUp, yawAngle));
+                currentYaw += yawAngle;
+            }
 
-            // Roll rotation
-            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
-            {
-                cameraUp = Vector3.Transform(cameraUp,
-                Matrix.CreateFromAxisAngle(cameraDirection,
-                MathHelper.PiOver4 / 45));
-            }
-            if (Mouse.GetState().RightButton == ButtonState.Pressed)
-            {
-                cameraUp = Vector3.Transform(cameraUp,
-                Matrix.CreateFromAxisAngle(cameraDirection, -MathHelper.PiOver4 / 45));
-            }
+            //// Roll rotation
+            //if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            //{
+            //    cameraUp = Vector3.Transform(cameraUp,
+            //    Matrix.CreateFromAxisAngle(cameraDirection,
+            //    MathHelper.PiOver4 / 45));
+            //}
+            //if (Mouse.GetState().RightButton == ButtonState.Pressed)
+            //{
+            //    cameraUp = Vector3.Transform(cameraUp,
+            //    Matrix.CreateFromAxisAngle(cameraDirection, -MathHelper.PiOver4 / 45));
+            //}
+
+            //// Pitch rotation
+            //cameraDirection = Vector3.Transform(cameraDirection,
+            //Matrix.CreateFromAxisAngle(Vector3.Cross(cameraUp, cameraDirection),
+            //(MathHelper.PiOver4 / 100) *
+            //(Mouse.GetState().Y - prevMouseState.Y)));
+
+            //cameraUp = Vector3.Transform(cameraUp,
+            //Matrix.CreateFromAxisAngle(Vector3.Cross(cameraUp, cameraDirection),
+            //(MathHelper.PiOver4 / 100) *
+            //(Mouse.GetState().Y - prevMouseState.Y)));
 
             // Pitch rotation
-            cameraDirection = Vector3.Transform(cameraDirection,
-            Matrix.CreateFromAxisAngle(Vector3.Cross(cameraUp, cameraDirection),
-            (MathHelper.PiOver4 / 100) *
-            (Mouse.GetState().Y - prevMouseState.Y)));
-
-            cameraUp = Vector3.Transform(cameraUp,
-            Matrix.CreateFromAxisAngle(Vector3.Cross(cameraUp, cameraDirection),
-            (MathHelper.PiOver4 / 100) *
-            (Mouse.GetState().Y - prevMouseState.Y)));
+            float pitchAngle = (MathHelper.PiOver4 / 150) *
+            (Mouse.GetState().Y - prevMouseState.Y);
+            if (Math.Abs(currentPitch + pitchAngle) < totalPitch)
+            {
+                cameraDirection = Vector3.Transform(cameraDirection,
+                Matrix.CreateFromAxisAngle(
+                Vector3.Cross(cameraUp, cameraDirection),
+                pitchAngle));
+                currentPitch += pitchAngle;
+            }
 
             // Reset prevMouseState
             prevMouseState = Mouse.GetState();
